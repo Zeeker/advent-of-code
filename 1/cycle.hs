c13569c66eb7d274f3df7a0b5957eea77282957f
Sascha Wolf <swolf.dev@gmail.com>
2018-12-02 21:14:43 +0100

    Cycle: Detect the first frequency which repeats

---
+++ 1/cycle.hs
@@ -0,0 +1,38 @@
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+main :: IO ()
+main = do
+  changes <- getChanges
+
+  let infiniteChanges = cycle changes
+  case detectDuplicate infiniteChanges of
+    Nothing -> putStrLn "No duplicated frequency detected!"
+    Just duplicate -> putStrLn $ "Duplicate: " ++ (show duplicate)
+
+getChanges :: IO [Int]
+getChanges = do
+  maybeChange <- getLine
+  case parseChange maybeChange of
+    Nothing -> return []
+    Just aChange -> do
+      moreChanges <- getChanges
+      return (aChange : moreChanges)
+
+parseChange :: String -> Maybe Int
+parseChange ('-':num) = Just $ -1 * (read num)
+parseChange ('+':num) = Just $ read num
+parseChange _ = Nothing
+
+detectDuplicate :: [Int] -> Maybe Int
+detectDuplicate = detectDuplicate' Set.empty 0
+
+detectDuplicate' :: Set Int -> Int -> [Int] -> Maybe Int
+detectDuplicate' _ _ [] = Nothing
+detectDuplicate' seen frequency (change : changes) =
+  if Set.member frequency seen
+    then Just frequency
+    else
+      let seen' = Set.insert frequency seen
+          frequency' = frequency + change
+      in detectDuplicate' seen' frequency' changes
