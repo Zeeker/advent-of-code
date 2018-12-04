4850242f731562b4144a59b39ce5df6925114b8f
Sascha Wolf <swolf.dev@gmail.com>
2018-12-04 22:25:47 +0100

    Day 2 - Checksum: Next incorrect solution

--- 02/checksum.hs
+++ 02/checksum.hs
@@ -1,13 +1,14 @@
-import           Data.List
+import           Data.Set (Set)
+import qualified Data.Set as Set
 
 main :: IO ()
 main = do
   boxIds <- getBoxIds
 
-  let twoCharsCount = length $ filter (hasAtLeastDuplicateChars 2) boxIds
-      threeCharsCount = length $ filter (hasAtLeastDuplicateChars 3) boxIds
+  let twoCharsCount = length $ filter (hasExactlyDuplicateChars 2) boxIds
+      threeCharsCount = length $ filter (hasExactlyDuplicateChars 3) boxIds
 
-  print $ (show twoCharsCount) ++ " * " ++ (show threeCharsCount) ++ " = " ++ (show $ twoCharsCount + threeCharsCount)
+  putStrLn $ (show twoCharsCount) ++ " * " ++ (show threeCharsCount) ++ " = " ++ (show $ twoCharsCount + threeCharsCount)
 
 getBoxIds :: IO [String]
 getBoxIds = do
@@ -23,17 +24,14 @@ parseBoxId :: String -> Maybe String
 parseBoxId []    = Nothing
 parseBoxId boxId = Just boxId
 
-hasAtLeastDuplicateChars :: Int -> [Char] -> Bool
-hasAtLeastDuplicateChars 0 _ = True
-hasAtLeastDuplicateChars _ [] = False
-hasAtLeastDuplicateChars 1 [_] = True
-hasAtLeastDuplicateChars _ [_] = False
-hasAtLeastDuplicateChars count (char:chars) = hasAtLeastDuplicateChars' count 0 char $ sort chars
-
-hasAtLeastDuplicateChars' :: Int -> Int -> Char -> [Char] -> Bool
-hasAtLeastDuplicateChars' count current char (char':chars)
-  | current >= count = True
-  | char == char' = hasAtLeastDuplicateChars' count (current + 1) char chars
-  | otherwise = hasAtLeastDuplicateChars' count 0 char' chars
-
-hasAtLeastDuplicateChars' _ _ _ [] = False
+hasExactlyDuplicateChars :: Int -> [Char] -> Bool
+hasExactlyDuplicateChars 0 _ = True
+hasExactlyDuplicateChars _ [] = False
+hasExactlyDuplicateChars 1 [_] = True
+hasExactlyDuplicateChars _ [_] = False
+hasExactlyDuplicateChars count chars =
+  let countOccurences char = length $ filter (== char) chars
+      uniqueChars = Set.toList $ Set.fromList chars
+      counts = map countOccurences uniqueChars
+      maxCount = foldl max 0 counts
+   in maxCount == count
