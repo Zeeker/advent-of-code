192a951315325156d3c65eec38124d69e8754786
Sascha Wolf <swolf.dev@gmail.com>
2018-12-04 22:49:59 +0100

    Day 2 - Checksum: I'm stupid, I added instead of multiplied

--- 02/checksum.hs
+++ 02/checksum.hs
@@ -8,7 +8,7 @@ main = do
   let twoCharsCount = length $ filter (hasExactlyDuplicateChars 2) boxIds
       threeCharsCount = length $ filter (hasExactlyDuplicateChars 3) boxIds
 
-  putStrLn $ (show twoCharsCount) ++ " * " ++ (show threeCharsCount) ++ " = " ++ (show $ twoCharsCount + threeCharsCount)
+  putStrLn $ (show twoCharsCount) ++ " * " ++ (show threeCharsCount) ++ " = " ++ (show $ twoCharsCount * threeCharsCount)
 
 getBoxIds :: IO [String]
 getBoxIds = do
@@ -25,13 +25,14 @@ parseBoxId []    = Nothing
 parseBoxId boxId = Just boxId
 
 hasExactlyDuplicateChars :: Int -> [Char] -> Bool
-hasExactlyDuplicateChars 0 _ = True
-hasExactlyDuplicateChars _ [] = False
-hasExactlyDuplicateChars 1 [_] = True
-hasExactlyDuplicateChars _ [_] = False
-hasExactlyDuplicateChars count chars =
-  let countOccurences char = length $ filter (== char) chars
+hasExactlyDuplicateChars 0 _         = True
+hasExactlyDuplicateChars _ []        = False
+hasExactlyDuplicateChars 1 [_]       = True
+hasExactlyDuplicateChars _ [_]       = False
+hasExactlyDuplicateChars count chars = count `elem` (countUniqueOccurences chars)
+
+countUniqueOccurences :: [Char] -> [Int]
+countUniqueOccurences chars =
+  let countOccurences chars char = length . filter (== char) $ chars
       uniqueChars = Set.toList $ Set.fromList chars
-      counts = map countOccurences uniqueChars
-      maxCount = foldl max 0 counts
-   in maxCount == count
+   in map (countOccurences chars) uniqueChars
